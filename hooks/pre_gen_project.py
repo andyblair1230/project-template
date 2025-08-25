ead2d9d3b15c44ff4064600d15697ab3aa6dc5fd
andyblair1230 <jablair1230@gmail.com>
2025-08-25 15:06:52 -0400

    pre-gen: abort if target directory exists

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 name = "{{ cookiecutter.repo_name }}"
 if os.path.exists(name):
     print(f"ERROR: Directory '{name}' already exists. Choose a different repo_name.")
